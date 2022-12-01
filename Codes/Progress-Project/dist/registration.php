3243eefb1a68ab880856ad010447d2c8fa90f0f7
hushnams <91113317+hushnams@users.noreply.github.com>
2022-12-01 11:01:05 +0800

    Add files via upload

---
+++ Codes/Progress-Project/dist/registration.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Registration
+  </title>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css'>
+<link rel='stylesheet' href='https://unicons.iconscout.com/release/v2.1.9/css/unicons.css'><link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<a href="https://front.codes/" class="logo" target="_blank">
+		<img src="https://assets.codepen.io/1462889/fcy.png" alt="">
+	</a>
+									<div class="center-wrap">
+										<div class="section text-center">
+											<h4 class="mb-4 pb-3">Sign Up</h4>
+											<div class="form-group">
+												<input type="text" name="firstName" class="form-style2" placeholder="Enter First Name" id="firstname" autocomplete="off">
+												<i class="input-icon uil uil-user"></i>
+											</div>	
+											<div class="form-group mt-2">
+												<input type="email" name="lastName" class="form-style2" placeholder="Enter Last Name " id="lastName" autocomplete="off">
+												<i class="input-icon uil uil-user"></i>
+											</div>	
+											<div class="form-group mt-2">
+												<input type="text" name="address" class="form-style2" placeholder="Enter Address" id="address" autocomplete="off">
+												<i class="input-icon uil uil-location-point"></i>
+											</div>
+
+											<div class="form-group mt-2">
+												<input type="text" name="contactNum" class="form-style2" placeholder="Enter Contact No." id="contactNum" autocomplete="off">
+												<i class="input-icon uil uil-phone-alt"></i>
+											</div>
+
+                                            <div class="form-group mt-2">
+												<input type="text" name="username" class="form-style2" placeholder="Enter Username" id="username" autocomplete="off">
+												<i class="input-icon uil uil-user-circle"></i>
+											</div>
+
+                                            <div class="form-group mt-2">
+												<input type="text" name="password" class="form-style2" placeholder="Enter Password" id="password" autocomplete="off">
+												<i class="input-icon uil uil-lock-alt"></i>
+											</div>
+
+                                            <div class="form-group mt-2">
+												<input type="text" name="confirmpassword" class="form-style2" placeholder="Confirm Password" id="confirmpassword" autocomplete="off">
+												<i class="input-icon uil uil-lock-alt"></i>
+											</div>
+                                            <a href="#" class="btn mt-4">submit</a>
+				      					</div>
+			      					</div>
+			      				</div>
+			      			</div>
+			      		</div>
+			      	</div>
+		      	</div>
+	      	</div>
+	    </div>
+	</div>
+<!-- partial -->
+  <script  src="./script.js"></script>
+
+</body>
+</html>
